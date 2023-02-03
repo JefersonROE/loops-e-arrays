16e3b58f5abb3bc900dd1e1eade4d5f5e9569d21
Jeferon 1 <jefersonrodriguesoe@gmail.com>
2023-02-03 13:29:14 -0300

    Atualização comandos java

---
+++ src/Ex1_OrdemInversa.java
@@ -0,0 +1,18 @@
+/*
+Crie um vetor de 6 números inteiros
+e mostre-os na ordem inversa.
+*/
+
+public class Ex1_OrdemInversa {
+    public static void main(String[] args) {
+        int []vetor = {1,2,3,4,5,6};
+        int []inversor;
+        int controle2 = vetor.length, controle3 = 0;
+
+        while (int i=0; i<=vetor.length; i++) {
+            [controle3]inversor = vetor[controle2];
+            controle2--;
+            controle3++;
+        }
+    }
+}
