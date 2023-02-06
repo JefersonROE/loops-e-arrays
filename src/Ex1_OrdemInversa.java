93fbb2ec7cfd4c5d3647c106586812dbca1d6fe7
Jeferon 1 <jefersonrodriguesoe@gmail.com>
2023-02-06 13:38:19 -0300

    Atualização comandos java

--- src/Ex1_OrdemInversa.java
+++ src/Ex1_OrdemInversa.java
@@ -6,13 +6,17 @@ e mostre-os na ordem inversa.
 public class Ex1_OrdemInversa {
     public static void main(String[] args) {
         int []vetor = {1,2,3,4,5,6};
-        int []inversor;
-        int controle2 = vetor.length, controle3 = 0;
+        int contador = 0;
 
-        while (int i=0; i<=vetor.length; i++) {
-            [controle3]inversor = vetor[controle2];
-            controle2--;
-            controle3++;
+        System.out.print("Vetor: ");
+        while (contador < (vetor.length)) {
+            System.out.print(vetor[contador]);
+            contador++;
+        }
+
+        System.out.print("\nVator Inverso: ");
+        for (int i = (vetor.length -1); i>=0; i--){
+            System.out.print(vetor[i] + " ");
         }
     }
 }
